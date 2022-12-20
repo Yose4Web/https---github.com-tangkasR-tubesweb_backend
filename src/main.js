d6702e486d74e5f168aa39e0b76fde725c571194
Yose4Web <yoseforweb@gmail.com>
2022-12-20 07:18:45 +0700

    Vue 2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from "@/plugins/vuetify";
+import router from "./router";
+import VueSimpleAlert from "vue-simple-alert";
 
-Vue.config.productionTip = false
+Vue.use(VueSimpleAlert);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
